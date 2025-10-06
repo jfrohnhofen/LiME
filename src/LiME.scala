712f7f054ec9b8e75310d071f56862a76b635f74
Johannes Frohnhofen <johannes@frohnhofen.com>
2025-10-06 16:43:42 +0200

    streaming memory content over UART is working

---
+++ src/LiME.scala
@@ -0,0 +1,62 @@
+package lime
+
+import spinal.core._
+import spinal.lib._
+import spinal.lib.com.uart._
+
+class LiME extends Component {
+  val io = new Bundle {
+    val uart_tx = out Bool ()
+  }
+  noIoPrefix()
+
+  val uart = new UartTx(baudRate = 115200, dataLength = 8)
+  io.uart_tx := uart.io.tx
+
+  val mem = new MemToStream()
+  uart.io.write <> mem.io.output
+
+  val timer = CounterFreeRun(ClockDomain.current.frequency.getValue.toInt)
+  mem.io.start := timer.willOverflow
+}
+
+class MemToStream extends Component {
+  val io = new Bundle {
+    val start = in Bool ()
+    val output = master Stream (Bits(8 bits))
+  }
+
+  val mem = Mem(Bits(8 bits), wordCount = 2048)
+  mem.init(Seq.tabulate(2048)(i => i % 256))
+
+  val address = Reg(UInt(log2Up(mem.wordCount) bits)) init (0)
+  val active = Reg(Bool()) init (False)
+
+  when(io.start && !active) {
+    active := True
+    address := 0
+  }
+
+  val memData = mem.readSync(address)
+
+  io.output.valid := active
+  io.output.payload := memData
+
+  when(io.output.fire) {
+    address := address + 1
+    when(address === mem.wordCount - 1) {
+      active := False
+    }
+  }
+}
+
+object LiME extends App {
+  SpinalConfig(
+    defaultConfigForClockDomains = ClockDomainConfig(
+      clockEdge = RISING,
+      resetKind = BOOT,
+      resetActiveLevel = LOW
+    ),
+    defaultClockDomainFrequency = FixedFrequency(25 MHz)
+  ).generateVerilog(new LiME())
+}
---
+++ src/UartTx.scala
@@ -0,0 +1,62 @@
+package lime
+
+import spinal.core._
+import spinal.lib._
+
+class UartTx(baudRate: Int, dataLength: Int) extends Component {
+  object State extends SpinalEnum {
+    val IDLE, START, DATA, STOP = newElement()
+  }
+  import State._
+
+  val io = new Bundle {
+    val tx = out Bool ()
+    val write = slave Stream (Bits(dataLength bit))
+  }
+
+  val ticksPerBit = (ClockDomain.current.frequency.getValue / baudRate).toInt
+  val counter = CounterFreeRun(ticksPerBit)
+  val bitIndex = Reg(UInt(log2Up(dataLength) bits))
+  val payload = Reg(Bits(dataLength bit))
+
+  val state = RegInit(IDLE)
+  io.write.ready := state === IDLE
+
+  switch(state) {
+    is(IDLE) {
+      io.tx := True
+      when(io.write.fire) {
+        payload := io.write.payload
+        state := START
+        counter.clear()
+      }
+    }
+
+    is(START) {
+      io.tx := False
+      when(counter.willOverflow) {
+        state := DATA
+        counter.clear()
+        bitIndex := 0
+      }
+    }
+
+    is(DATA) {
+      io.tx := payload(bitIndex)
+      when(counter.willOverflow) {
+        when(bitIndex === dataLength - 1) {
+          state := STOP
+        }
+        bitIndex := bitIndex + 1
+        counter.clear()
+      }
+    }
+
+    is(STOP) {
+      io.tx := True
+      when(counter.willOverflow) {
+        state := IDLE
+      }
+    }
+  }
+}
