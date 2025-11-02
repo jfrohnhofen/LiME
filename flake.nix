992d9520a1b56e0afe08c6b87e79b2e21efa3224
Johannes Frohnhofen <johannes@frohnhofen.com>
2025-11-02 11:10:32 +0100

    nix flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Light in Motion Engine";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            yosys
+            nextpnr
+            trellis
+            openfpgaloader
+            gtkwave
+            verilator
+          ];
+        };
+      }
+    );
+}
