68a143c7bf9546a1f0461039ba18c9f1bfc6b74a
Johannes Frohnhofen <johannes@frohnhofen.com>
2026-03-31 11:31:55 +0200

    UartTx component

---
+++ src/testing/UartTxTest.scala
@@ -0,0 +1,21 @@
+package lime.testing
+
+import lime.util._
+import spinal.core._
+import spinal.lib._
+
+class UartTxTest extends Component {
+  val io = new Bundle {
+    val uart_tx = out Bool()
+  }
+  noIoPrefix()
+
+  val uart = new UartTx(baudRate = 1_000_000, dataLength = 8)
+  io.uart_tx := uart.io.tx
+
+  // Send incrementing bytes continuously
+  val counter = Reg(UInt(8 bits)) init 0
+  uart.io.write.valid   := True
+  uart.io.write.payload := counter.asBits
+  when(uart.io.write.fire) { counter := counter + 1 }
+}
